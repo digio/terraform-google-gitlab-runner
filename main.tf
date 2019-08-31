8b72c77366ee339b47fa71ef59c2d4fab0d498fe
Brett Henderson <brett@bretth.com>
2019-08-31 16:32:40 +1000

    remove unnecessary string interpolation

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "google_service_account" "ci-worker" {
 
 # Allow GitLab CI runner to use the worker service account.
 resource "google_service_account_iam_member" "ci-worker-ci-runner" {
-  service_account_id = "${google_service_account.ci-worker.name}"
+  service_account_id = google_service_account.ci-worker.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.ci-runner.email}"
 }
