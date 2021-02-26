214dfb0e92ebd1fea1f95d82bf627bd59ed58cff
Brett Henderson <brett@bretth.com>
2021-02-26 17:26:44 +1100

    update copyright statement to year 2021

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Mantel Group Pty Ltd
+ * Copyright 2021 Mantel Group Pty Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
